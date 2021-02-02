384d428150d0c96ce82a4217052452ffc0592b36
Digolas.Forge <42858482+rsbb0818@users.noreply.github.com>
2021-02-02 17:32:46 +1100

    Updates ports

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const { MONGODB } = require('./config');
 
 const pubsub = new PubSub();
 
+const PORT = process.env.port || 5000;
+// This is for actual deployment server ->*
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
@@ -22,10 +25,15 @@ mongoose
 	//Without 'useUnifiedTopology: true,' will cause => DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, and will be removed in a future version.
 	.then(() => {
 		console.log('MongoDB Connected');
-		return server.listen({ port: 5000 });
+		return server.listen({ port: Port });
+		// return server.listen({ port: 5000 });
+		// * -> this is for localhost:5000
 	})
 	.then((res) => {
 		console.log(`server running at ${res.url}`);
+	})
+	.catch((err) => {
+		console.error(err);
 	});
 
 // Apollo server actually runs Express at the back, can be seen in the node_module which included after install apollo-server
